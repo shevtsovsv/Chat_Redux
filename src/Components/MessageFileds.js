04adda92263e5562bb0e355d0008c59bc45ebb62
Yakov <simonov_iakov@mail.ru>
2021-05-03 13:03:11 +0300

    2021_05_03 Project start

---
+++ src/Components/MessageFileds.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const MessageFields = () => {
+    return (
+        <div>
+            <div>MFields</div>
+        </div>
+    )
+}
+
+export default MessageFields;
